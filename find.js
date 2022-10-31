2d583825305240fc3d6d720feb51471f7878e7b8
0xNB-dev <banish555@gmail.com>
2022-10-31 18:58:04 +0200

    Update find.js

--- find.js
+++ find.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 const {Pair } = require( "./db.js")
 
-console.log("look-------------------------")
-
 
 async function find() {
   const res = await Pair.find({
@@ -10,7 +8,6 @@ async function find() {
   }, "addr");
 
   console.log(res);
-  console.log("look-------------------------")
   mongoose.connection.close("mongodb://localhost:27017/mongo") .then(() => console.log("MongoDB Disconnected"))
   .catch((err) => console.log(err));
 
